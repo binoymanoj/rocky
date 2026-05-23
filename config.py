bd5cdb6e7b774d09aed0834f04869b8856c961aa
binoymanoj <binoypm2002@gmail.com>
2026-05-23 17:05:06 +0530

    fix: missing variables added

--- config.py
+++ config.py
@@ -8,6 +8,10 @@ WHISPER_PATH = "/home/pi/Applications/whisper.cpp/build/bin/main"
 WHISPER_MODEL = "/home/pi/Applications/whisper.cpp/models/ggml-base.en.bin"
 PIPER_VOICE = "/home/pi/rocky/voices/en_US-arctic-medium.onnx"
 
+# Directories
+RECORDINGS_DIR = "recordings"
+RESPONSES_DIR = "responses"
+
 # LLM
 OLLAMA_MODEL = "gemma3:4b"
 OLLAMA_URL = "http://localhost:11434"
